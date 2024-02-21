6b8bc8855fe57b0bd8f98b827fef61430e653409
Siphephelo Myeni <sipheanele@gmail.com>
2024-02-21 19:58:16 +0200

    New Controller created

---
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.API.Controllers
+{
+    /*https://localhost:xxxx/api/catogories
+     Should we want to reach this controller
+    */
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+    }
+}
