ed85d327b9ebb38366525644875caa4d623a8bfc
Siphephelo Myeni <sipheanele@gmail.com>
2024-02-21 15:35:31 +0200

    Injecting Dbcontext into the application

--- CodePulse.API/Program.cs
+++ CodePulse.API/Program.cs
@@ -1,3 +1,6 @@
+using CodePulse.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+/*Injecting the dbcontext, which is of type application dbcontext inside the Program.cs file
+which is the application itself, so that we can later on use the application Dbcontext to
+Communicate with the database from the controller or the repository*/
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CodePulseConnectionString"));
+});
 
 var app = builder.Build();
 
